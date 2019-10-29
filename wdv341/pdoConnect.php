75f78e218aacb92ac093b666e3cb9ce77c305712
rb_ <41527196+EndaHallahan@users.noreply.github.com>
2019-10-29 13:41:56 -0500

    PDO connect

---
+++ wdv341/pdoConnect.php
@@ -0,0 +1,17 @@
+<?php
+$servername = "localhost";
+$username = "php";
+$password = "TestPass123";
+$database = "wdv341";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+?>
